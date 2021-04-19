f57f2c4e08c4ad8ede9d7d479e48a267cb660407
Tobias Pflug <tobias.pflug@gmail.com>
2021-04-19 17:43:38 +0200

    WIP: hack hack hack

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,9 +2,10 @@
 
 set +x
 
-ls machines.json
-ls plutus_playground.*.conf
 
 ssh-add -l
 terraform apply -auto-approve -target local_file.machines -target local_file.ssh_config ./terraform/
+cat machines.json
+cat plutus_playground.*.conf
+
 deploy-nix
