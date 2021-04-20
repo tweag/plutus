5c8ea647fc3dfaa1d55588dffc3f8ff2b89ef681
Tobias Pflug <tobias.pflug@gmail.com>
2021-04-20 16:59:46 +0200

    Test ssh'ing all servers

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 terraform apply -auto-approve -target local_file.machines -target local_file.ssh_config ./terraform/
+
+ssh -F ./plutus_playground.tobias.conf playgrounds-a.internal.tobias.plutus.iohkdev.io hostname
+ssh -F ./plutus_playground.tobias.conf playgrounds-b.internal.tobias.plutus.iohkdev.io hostname
+ssh -F ./plutus_playground.tobias.conf marlowe-dash-a.internal.tobias.plutus.iohkdev.io hostname
+ssh -F ./plutus_playground.tobias.conf marlowe-dash-b.internal.tobias.plutus.iohkdev.io  hostname
+ssh -F ./plutus_playground.tobias.conf webghc-a.internal.tobias.plutus.iohkdev.io hostname
+ssh -F ./plutus_playground.tobias.conf webghc-a.internal.tobias.plutus.iohkdev.io hostname
+
 deploy-nix
