3e9658dfddfa891a7722f4d8de4e0edb2decc88e
Tobias Pflug <tobias.pflug@gmail.com>
2021-04-20 16:35:42 +0200

    Remove debug code

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env bash
-
-set -x
-set -e
-
 terraform apply -auto-approve -target local_file.machines -target local_file.ssh_config ./terraform/
-
-echo "**************************************************"
-ssh-add -l
-ls ./plutus_playground.tobias.conf
-ssh -V
-ssh -v -F ./plutus_playground.tobias.conf playgrounds-a.internal.tobias.plutus.iohkdev.io hostname
-echo "**************************************************"
-
-cat machines.json
-cat plutus_playground.*.conf
-
 deploy-nix
