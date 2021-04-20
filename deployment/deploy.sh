953b597e758b37520a8b3df571372fd7819ac2fd
Tobias Pflug <tobias.pflug@gmail.com>
2021-04-20 13:07:54 +0200

    deploy.sh: debugging

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,7 +3,13 @@
 set -x
 set -e
 
+echo "**************************************************"
 ssh-add -l
+ls ./plutus_playground.tobias.conf
+ssh --version
+ssh -F ./plutus_playground.tobias.conf playgrounds-a.internal.tobias.plutus.iohkdev.io hostname
+echo "**************************************************"
+
 terraform apply -auto-approve -target local_file.machines -target local_file.ssh_config ./terraform/
 cat machines.json
 cat plutus_playground.*.conf
